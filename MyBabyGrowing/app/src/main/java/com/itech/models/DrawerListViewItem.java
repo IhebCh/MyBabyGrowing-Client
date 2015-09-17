02f9607aa482bc18e3820835595503d359c1694f
oSunshine <bi_chorfi@esi.dz>
2015-09-17 23:06:36 +0200

    beginning

---
+++ MyBabyGrowing/app/src/main/java/com/itech/models/DrawerListViewItem.java
@@ -0,0 +1,26 @@
+package com.itech.models;
+
+/**
+ * Created by oSunshine on 23/06/2015.
+ */
+public class DrawerListViewItem {
+
+    private int iconId;
+    private String title;
+
+    public int getIconId() {
+        return iconId;
+    }
+
+    public void setIconId(int iconId) {
+        this.iconId = iconId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
